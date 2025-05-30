de69ce2908db8d69a837e3b5440dcc09d0863822
Tomas <tomasschool391@gmail.com>
2025-05-30 17:49:08 -0300

    agregue a los models: event y venue, el campo imagen. cree la carpeta static, donde se guardan las imagenes de los modelos

--- app/models/venue.py
+++ app/models/venue.py
@@ -10,7 +10,9 @@ class Venue(models.Model):
     city = models.CharField(max_length=150)
     capacity = models.IntegerField()
     contact = models.CharField(max_length=150)
-    
+    imagen = models.ImageField(upload_to='static/media/venues', null=True, blank=True)
+
+
     def __str__(self):
         return self.name
 
